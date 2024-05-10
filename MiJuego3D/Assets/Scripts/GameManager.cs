fd67ffc89d7479233311edc220841c9c5867152f
claudiaramos26 <158211261+claudiaramos26@users.noreply.github.com>
2024-05-09 22:32:30 -0600

    inicio

---
+++ MiJuego3D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int bestScore;
+    public int currentScore;
+
+    public int currentLevel = 0;
+
+    public static GameManager singleton;
+    
+    void Awake()
+    {
+        if (singleton==null)
+        {
+            singleton = this;
+        }
+        else if (singleton!=this)
+        {
+            Destroy(gameObject);
+        }
+
+        bestScore = PlayerPrefs.GetInt("HighScore");
+    }
+
+    
+    public void NextLevel()
+    {
+        currentLevel++;
+        FindObjectOfType<BallController>().ResetBall();
+        FindObjectOfType<HelixController>().LoadStage(currentLevel);
+
+        Debug.Log("Pasamos de nivel");
+    }
+
+    public void Restartlevel()
+    {
+        Debug.Log("Restart");
+        singleton.currentScore = 0;
+        FindObjectOfType<BallController>().ResetBall();
+        FindObjectOfType<HelixController>().LoadStage(currentLevel);
+    }
+
+    public void AddScore(int scoreToAdd)
+    {
+        currentScore += scoreToAdd;
+
+        if (currentScore>bestScore)
+        {
+            bestScore = currentScore;
+            PlayerPrefs.SetInt("HighScore",currentScore);
+        }
+    }
+
+}
